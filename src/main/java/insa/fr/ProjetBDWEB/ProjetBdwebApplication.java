9f6dc43e741f282679ffd212b55d87ed32326cd2
rihabMez <rihab.mez316@gmail.com>
2021-11-27 10:03:40 +0100

    Ajout du projet Spring boot

---
+++ src/main/java/insa/fr/ProjetBDWEB/ProjetBdwebApplication.java
@@ -0,0 +1,13 @@
+package insa.fr.ProjetBDWEB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetBdwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetBdwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/insa/fr/ProjetBDWEB/ProjetBdwebApplicationTests.java
@@ -0,0 +1,13 @@
+package insa.fr.ProjetBDWEB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetBdwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
